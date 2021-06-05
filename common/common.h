9622a09643aafd52ab927651b2c15f86f0287130
LiJunQiang <ljqchina@163.com>
2021-06-05 08:39:17 +0800

    fix: add common.h

---
+++ common/common.h
@@ -0,0 +1,17 @@
+#ifndef COMMON_H_
+#define COMMON_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string>
+
+#include <event2/bufferevent.h>
+#include <event2/buffer.h>
+#include <event2/listener.h>
+#include <event2/util.h>
+#include <event2/event.h>
+#include <event2/event_struct.h>
+
+#endif
+
